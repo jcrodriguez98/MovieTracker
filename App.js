d99a1b91892f22cbe4938cb991573fd88d9042f0
Carlos Rodriguez <c.rodriguez2016@gmail.com>
2023-04-20 16:22:13 -0500

    added dependencies and icons

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
+import {useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import { Ionicons } from '@expo/vector-icons';
+
+function Items() {
+  const [items, setItems] = useState(null);
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        
+      </ScrollView>
+      <Ionicons name='add-circle' size={24} color='green' />
       <StatusBar style="auto" />
     </View>
   );
