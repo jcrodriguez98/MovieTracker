b7a2a40af5e64c8fd784f50634c4d0c5802510e7
Carlos Rodriguez <c.rodriguez2016@gmail.com>
2023-04-21 22:07:35 -0500

    added database

--- App.js
+++ App.js
@@ -4,18 +4,68 @@ import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-nati
 import * as SQLite from 'expo-sqlite';
 import { Ionicons } from '@expo/vector-icons';
 
-function Items() {
-  const [items, setItems] = useState(null);
+function openDatabase() {
+  if (Platform.OS === "web") {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        };
+      },
+    };
+  }
+
+const db = SQLite.openDatabase("db.db");
+  return db;
 }
 
+const db = openDatabase();
+
+function Items() {
+  const [items, setItems] = useState(null);
+
+  return ( 
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionHeading}>{heading}</Text>
+      {items.map(({ id, done, value, itemDate }) => (
+        <TouchableOpacity
+          key={id}
+          onPress={() => onPressItem && onPressItem(id)}
+          style={{
+            backgroundColor: done ? "#1c9963" : "#fff",
+            borderColor: "#000",
+            borderWidth: 1,
+            padding: 8,
+          }}
+        >
+          <Text style={{ color: done ? "#fff" : "#000" }}>{itemDate}:  {value}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
 
 export default function App() {
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "drop table movies;"
+      );
+      tx.executeSql(
+        "create table if not exists movies (id integer primary key not null, movieName text);"
+      );
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <ScrollView>
-        
+        <Items>
+
+        </Items>
       </ScrollView>
-      <Ionicons name='add-circle' size={24} color='green' />
+      <Ionicons style={styles.addButton} name='add-circle' size={100} color='green' />
       <StatusBar style="auto" />
     </View>
   );
@@ -28,4 +78,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  addButton: {
+    padding: 20
+  }
 });
